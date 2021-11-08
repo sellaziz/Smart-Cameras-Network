a096c331736d7c877bf71a9cac4204f49addc96b
Elzizou <sellaziz@hotmail.fr>
2021-11-08 16:32:52 +0100

    add library for config

--- communication/smart-cam_simple_server.py
+++ communication/smart-cam_simple_server.py
@@ -1,34 +1,7 @@
 #!/usr/bin/env python3
 
 import socket
-import configparser
-import os
-
-def read_config(path):
-    if os.path.isfile(path):
-        config = configparser.ConfigParser()
-        config.read(path)
-        hostname           = config['SERVER']['Hostname']
-        port               = int(config['SERVER']['Port'])
-        buffersize         = int(config['SERVER']['buffersize'])
-        client_name        = config['CLIENT']['Name']
-        client_orientation = config['CLIENT']['orientation']
-        return hostname, port, buffersize, client_name, client_orientation
-    else:
-        print("Config File do not exist please Edit :", path)
-        create_default_config(path)
-        return None
-
-def create_default_config(path):
-    config = configparser.ConfigParser()
-    config['SERVER'] = {'Hostname': 'localhost',
-                        'Port': '65432',
-                        'buffersize': '1024'
-                        }
-    config['CLIENT'] = {'Name': 'localhost',
-                        'orientation': 'none'}
-    with open(path, 'w') as configfile:
-        config.write(configfile)
+from comm_config import read_config
 
 def client_send_message(hostname, port, buffersize):
     """
