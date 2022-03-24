bf1c1479e465a53d984571920796fdd873da8ac4
Zijie NING <zijie.ning@imt-atlantique.net>
2022-03-24 16:50:37 +0100

    Count_dataset
    Count the number of pictures in each class

---
+++ count_data.py
@@ -0,0 +1,27 @@
+'''
+@Name           :count_data.py
+@Description    :
+@Time           :2022/03/07 16:39:53
+@Author         :Zijie NING
+@Version        :1.0
+
+Count the number of pictures in each class.
+'''
+
+
+import os
+
+count = [0, 0, 0, 0, 0, 0, 0]
+
+path = "/homes/z20ning/Bureau/Local/smart_cam/data/obj_train_data"
+dirs = os.listdir(path)
+for i in dirs:
+    if os.path.splitext(i)[1] == ".txt":
+        f = open(path + "/" + i)
+        # print(f)
+        for line in f:
+            i = int(line[0])
+            print(line)
+            count[i] += 1
+        f.close()
+print(count)
